482ed66f09ccff70a48f97f44fcb950a6fc986f0
Lindy Briggs <briggs.lindy@yahoo.com>
2022-05-10 11:03:14 -0500

    forgot to remove name for signup

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,13 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && username && password) {
+  if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, username, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
