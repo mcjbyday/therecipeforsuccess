95b11b9348245e8095fb51daf813e32402b8fdb3
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-05-11 15:02:23 -0500

    got checkbox automation working on pageload

--- public/js/filterMeals.js
+++ public/js/filterMeals.js
@@ -1,42 +1,55 @@
-
-// grab the criteria DONE
-// store in an array; join with ampersands, DONE
-// send to that route with a query
-
-let myFood = {
-    isGlutenFree: true,
-    isNutFree: false,
-    isVegan: true
-}
-
-var reqString = `?`;
-
-for (const property in myFood) {
-    console.log(`${property}: ${myFood[property]}`);
-  if (myFood[property]) {
-  	reqString += `&${property}=${myFood[property]}`;
-  }
-}
-console.log(reqString);
-// ?isGlutenFree=true&isVegan=true
-
-// document.querySelector('.submitBtn').addEventListener('click', {
+function setCheckBoxesFromURL() {
+    // build an array of the parameters for the filter
+    const myCriteria = document.location.search.slice(1).split("&")
     
+    // create an array consisting only of the criteria to be checked on start
+    myCheckboxes = myCriteria.map((criteria) => {
+        return criteria.split("=")[0]    
+      })
+    
+    // set the value of each checkbox on page load
+    for (let i=0; i<myCheckboxes.length; i++) {
+        document.querySelector(`[name="${myCheckboxes[i]}"]`).checked = true;
+    }
+
+}
+
+const preferencesBtnHandler = async (event) => {
     // snippet for getting the checked or not checked value of a checkbox
-        // isGlutenFree: document.querySelector('#glutenFreeCheckBox').value(),
-        // isNutFree = document.querySelector('#nutFreeCheckBox').value(), 
-    // isVegan
-    // isDairyFree
-    // isPescatarian
-    // isVegetarian
-    // document.location.replace('/' + reqString);
+
+    const myFood = {
+        isDairyFree: document.querySelector('#dairyFreeCheckBox').checked, 
+        isGlutenFree: document.querySelector('#glutenFreeCheckBox').checked,
+        isNutFree: document.querySelector('#nutFreeCheckBox').checked, 
+        isPescatarian: document.querySelector('#pescatarianCheckBox').checked, 
+        isVegan: document.querySelector('#veganCheckBox').checked, 
+        isVegetarian: document.querySelector('#vegetarianCheckBox').checked, 
+    }
+    await console.log(myFood);
+    var reqString = `?`;
+
+    for (const property in myFood) {
+        // console.log(`${property}: ${myFood[property]}`);
+        if (myFood[property]) {
+            reqString += `${property}=${myFood[property]}&`;
+        }
+    }
+
+    reqString = reqString.slice(0, -1);
+    // await console.log(reqString + " after");
+
+    document.location.replace('/' + reqString);
+
+}
+
+const viewAllHandler = async (event) => {
+    document.location.replace('/');
+}
+
+setCheckBoxesFromURL();
+
+
+document.querySelector('#viewAllButton').addEventListener('click', viewAllHandler);
     
-// })
+document.querySelector('#preferencesBtn').addEventListener('click', preferencesBtnHandler);
 
-
-// id="glutenFreeCheckBox"
-// id="nutFreeCheckBox"
-// id="veganCheckBox"
-// id="dairyFreeCheckBox"
-// id="pescatarianCheckBox"
-// id="vegetarianCheckBox"
